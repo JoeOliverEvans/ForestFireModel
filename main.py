6b7c7ab8a91e1b532f033b24e18d70a8e34b5776
joeol <joeoliverevans@gmail.com>
2022-11-25 14:01:58 +0000

    created function to examine number of trees etc.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from matplotlib import animation
 from matplotlib import colors
 import matplotlib
 
-matplotlib.use("TkAgg")
-
 
 def timestep(forest):
     """
@@ -17,7 +15,7 @@ def timestep(forest):
     random = np.random.random(shape)
     new_forest = np.where(forest == tree, tree, 0)
     new_growth = np.where((forest == empty) & (random < growth_probability), tree, 0)
-    ignition = np.where((forest == tree) & (random < ignition_probability), tree, 0)
+    ignition = np.where((forest == tree) & (random < ignition_probability), 1, 0)
     new_forest = new_forest + new_growth + ignition
     for x in fires:
         for y in directions:
@@ -33,25 +31,58 @@ directions = [(0, -1), (-1, 0), (1, 0), (0, 1)]
 ignition_probability = 0.001
 growth_probability = 0.01
 shape = (100, 100)
-colour_list = colors.ListedColormap(['Black', 'Green', 'Red'])
-grid = np.zeros(shape)
 empty, tree, fire = 0, 1, 2
 
 
-for p in range(0, 100):
-    grid = timestep(grid)
+def visual():
+    matplotlib.use("TkAgg")
+    colour_list = colors.ListedColormap(['Black', 'Green', 'Red'])
+    grid = np.zeros(shape)
+    for p in range(0, 50):
+        grid = timestep(grid)
 
-fig, ax = plt.subplots()
-image = ax.imshow(grid, cmap=colour_list)
+    fig, ax = plt.subplots()
+    image = ax.imshow(grid, cmap=colour_list)
+
+    def animate(frame):
+        image.set_data(animate.grid)
+        animate.grid = timestep(animate.grid)
+
+    animate.grid = grid
+    interval = 50
+    amin = matplotlib.animation.FuncAnimation(fig, animate, interval=interval, frames=200)
+
+    plt.show()
 
 
-def animate(frame):
-    image.set_data(animate.grid)
-    animate.grid = timestep(animate.grid)
+def study_numbers(n=10**3):
+    """
+    Analyse the number of trees fires and empty squares over time
+    :param: number of iterations
+    :return: Nothing
+    """
+    grid = np.zeros(shape)
+    empties = [np.count_nonzero(grid == empty)]
+    trees = [np.count_nonzero(grid == tree)]
+    fires = [np.count_nonzero(grid == fire)]
+    for x in range(1, n):
+        grid = timestep(grid)
+        empties.append(np.count_nonzero(grid == empty))
+        trees.append((np.count_nonzero(grid == tree)))
+        fires.append((np.count_nonzero(grid == fire)))
+
+    xvalues = np.arange(0, n)
+
+    fig, ax1 = plt.subplots()
+
+    ax1.errorbar(xvalues, empties, label='empties', fmt='.', color='k')
+    ax1.errorbar(xvalues, trees, label='trees', fmt='.', color='green')
+    ax1.errorbar(xvalues, fires, label='fires', fmt='.', color='red')
+    ax1.set_xlabel('number of iterations')
+
+    plt.legend()
+    fig.tight_layout()
+    plt.show()
 
 
-animate.grid = grid
-interval = 50
-animation = matplotlib.animation.FuncAnimation(fig, animate, interval=interval, frames=200)
-
-plt.show()
+study_numbers()
