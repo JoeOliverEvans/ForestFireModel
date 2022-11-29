84b2f0345790fe0d5872604536d0578bbe09534d
joeol <joeoliverevans@gmail.com>
2022-11-29 16:00:40 +0000

    wrestling with labels and merging labels

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def timestep(forest):
 directions = [(0, -1), (-1, 0), (1, 0), (0, 1)]
 ignition_probability = 0.001
 growth_probability = 0.01
-shape = (10, 10)
+shape = (50, 50)
 empty, tree, fire = 0, 1, 2
 
 
@@ -105,15 +105,21 @@ def Hoshen_Kopelman(grid):
         return q
 
     largest_label = 0
-    label = np.zeros(shape)
-    labels = np.arange(0, shape[0]*shape[1])
+    label = np.zeros(shape, dtype=int)
+    labels = np.arange(0, (shape[0]*shape[1])/2)
     for xkop in range(0, shape[0]):
         for ykop in range(0, shape[1]):
             if grid[xkop, ykop] == tree:
-                left = grid[xkop - 1, ykop]
-                above = grid[xkop, ykop - 1]
+                if ykop-1<0:
+                    left = 0
+                else:
+                    left = grid[xkop, ykop-1]
+                if xkop-1<0:
+                    above=0
+                else:
+                    above = grid[xkop-1, ykop]
                 if left != tree and above != tree:
-                    largest_label += 1
+                    largest_label = largest_label + 1
                     label[xkop, ykop] = largest_label
                 elif left == tree and above != tree:
                     label[xkop, ykop] = find(left)
@@ -122,10 +128,88 @@ def Hoshen_Kopelman(grid):
                 else:
                     union(left, above)
                     label[xkop, ykop] = find(left)
+    '''for (int i=0; i < m; i++)
+        for (int j=0; j < n; j++)
+            if (matrix[i][j]) {
+            int x = uf_find(matrix[i][j]);
+            if (new_labels[x] == 0) {
+            new_labels[0]++;
+            new_labels[x] = new_labels[0];
+            }
+            matrix[i][j] = new_labels[x];
+            }
+
+    int
+    total_clusters = new_labels[0];
+
+    free(new_labels);'''
+    new_labels = np.arange(0,max(labels))
+    for xkop in range(0, shape[0]):
+        for ykop in range(0, shape[1]):
+            if grid[xkop, ykop] != 0:
+                x = find(label[xkop,ykop])
+                if new_labels[x] == 0:
+                    new_labels[0] += 1
+                    new_labels[x] = new_labels[0]
+                label[xkop,ykop] = new_labels[x]
     return label
 
 
-def Hoshen_Kopelman2():
+def Hoshen_Kopelman2(grid):
+    labeled = np.zeros(shape, dtype=int)
+    largest_label = int(0)
+    equivalent = []
+    for xkop in range(0, shape[0]):
+        for ykop in range(0, shape[1]):
+            if grid[xkop, ykop] == 1:
+                if ykop-1<0:
+                    left = 0
+                else:
+                    left = grid[xkop, ykop-1]
+                if xkop-1<0:
+                    above=0
+                else:
+                    above = grid[xkop-1, ykop]
+                if left != tree and above != tree:
+                    largest_label += 1
+                    labeled[xkop, ykop] = largest_label
+                if left == tree and above != tree:
+                    labeled[xkop, ykop] = labeled[xkop, ykop-1]
+                if left != tree and above == tree:
+                    labeled[xkop, ykop] = labeled[xkop-1, ykop]
+                if left == tree and above == tree:
+                    labeled[xkop, ykop] = labeled[xkop, ykop-1]
+                    if labeled[xkop-1, ykop] != labeled[xkop, ykop-1]:
+                        join = [labeled[xkop-1, ykop], labeled[xkop, ykop-1]]
+                        new = True
+                        for n, h in enumerate(equivalent):
+                            if join[0] in h and join[1] not in h:
+                                equivalent[n].append(join[1])
+                                new = False
+                            elif join[1] in h and join[0] not in h:
+                                equivalent[n].append(join[0])
+                                new = False
+                        if new:
+                            equivalent.append(join)
+    print(equivalent)
+    for pair in equivalent:
+        pair.sort()
+        for r in range(1, len(pair)):
+            labeled = np.where(labeled == pair[r], pair[0], labeled)
+    print(labeled)
+    for p in range(0, largest_label):
+        if p not in labeled:
+            np.where(labeled>p, labeled-1, labeled)
+    '''labeled[xkop, ykop] = labeled[xkop-1, ykop]
+    cond = True
+    p = 0
+    while cond:
+        p += 1
+        if labeled[xkop, ykop-1] != 0:
+            labeled[xkop, ykop-p] = labeled[xkop, ykop]
+        cond = False'''
+    return labeled
+
 
 
 #study_numbers()
@@ -134,4 +218,10 @@ for x in range(0, 50):
     grid = timestep(grid)
 
 print(grid)
-print(Hoshen_Kopelman2(grid))
+hosh = Hoshen_Kopelman2(grid)
+print(hosh)
+new = np.where(hosh != 0, 1, 0)
+print(np.max(np.where(grid==1, 1, 0)-new))
+
+
+
